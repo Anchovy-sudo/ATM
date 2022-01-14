027bd495cb6153291c07fa3011d5c62cd839c991
Fatih Yokus <yokusfat@gmail.com>
2022-01-15 00:01:41 +0300

    initial commit

---
+++ src/ATMExample.java
@@ -0,0 +1,40 @@
+import java.security.KeyStore;
+import java.util.Scanner;
+public class ATMExample {
+    public static void main(String[] args) {
+        int balance = 10000, withdraw, deposit;
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Automated Teller Machine");
+            System.out.println("Choose 1 for Withdraw");
+            System.out.println("Choose 2 for Deposit");
+            System.out.println("Choose 3 for Check Balance");
+            System.out.println("Choose 4 for EXIT");
+            System.out.print("Choose the operation you want to perform: ");
+
+            int choice = sc.nextInt();
+            switch (choice) {
+                case 1 -> {
+                    System.out.println("Enter money to be withdrawn: ");
+                    withdraw = sc.nextInt();
+                    if (balance >= withdraw) {
+                        balance = balance - withdraw;
+                        System.out.println("Please collect your money");
+                    } else {
+                        System.out.println("Insufficient balance");
+                    }
+
+                }
+                case 2 -> {
+                    System.out.println("Enter the money to be deposited: ");
+                    deposit = sc.nextInt();
+                    balance = balance + deposit;
+                    System.out.println("Your money has been successfully deposited");
+                }
+                case 3 -> System.out.println("Balance: " + balance);
+                case 4 -> System.exit(0);
+            }
+        }
+    }
+}
